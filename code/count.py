0af1c5b958c9e66f545a018bb85db9934ca998b7
huylg <huylg@vng.com.vn>
2020-04-23 12:30:42 +0700

    calculate PiaW and Psvw

--- code/count.py
+++ code/count.py
@@ -1,21 +1,29 @@
 import re
+from underthesea import sent_tokenize
 commonWordFile = open('../Thesis_Dataset/3000_most_word.txt',mode = 'r', encoding = 'utf-8')
 commonSyllableFile = open('../Thesis_Dataset/3000_most_syllable.txt', mode = 'r', encoding = 'utf-8')
+sinoVietWordFile = open('../Thesis_Dataset/sino_vietnamese.txt',mode='r',encoding='utf-8')
+dialectWordFile = open('../Thesis_Dataset/dialect.txt',mode='r',encoding='utf-8')
 
-inputFilePath = input()
-inputFile = open(inputFilePath,mode='r',encoding = 'utf-8')
+#inputFilePath = input()
+#inputFile = open(inputFilePath,mode='r',encoding = 'utf-8')
 
 rawCommonWordList = commonWordFile.read()
 rawCommonSyllableList = commonSyllableFile.read()
-rawInputData = inputFile.read()
+rawSinoVietWordSet = sinoVietWordFile.read()
+rawDialectWordSet = dialectWordFile.read()
+rawInputData = input()
 
 commonWordSet = set(rawCommonWordList.split('\n'))
 commonSyllableSet = set(rawCommonSyllableList.split('\n'))
- 
+sinoVietWordSet = set(rawSinoVietWordSet.split('\n'))
+dialectWordSet = set(rawDialectWordSet.split('\n'))
 sentenceMarkList = set([',','.','!','?',':',';','-'])
 
 #preprocesing
-sentenceList = rawInputData.split('\n')
+rawInputData = rawInputData.lower()
+
+sentenceList = sent_tokenize(text)
 
 #count index
 
@@ -49,7 +57,7 @@ print('asls', aslsIndex)
 
 # average senetence length in character
 
-def calculateASLC(sentenceList):
+def calculateASLC(sentenceList): 
     global sentenceMarkList
     result = 0
     amountOfSentence = len(sentenceList)
@@ -96,12 +104,92 @@ def calculateAWLS(SentenceList):
 awlsIndex = calculateAWLS(sentenceList)
 print('awls', awlsIndex)
 
+# percertange of difficult syllables
+
+def calculatePDS(sentenceList,commonSyllableList):
+    global sentenceMarkList
+    totalSyllable = 0
+    totalDifficultSyllable = 0
+
+    for sentence in sentenceList:
+        syllableList = list(filter(lambda x: x and not  (x in sentenceMarkList) ,re.split(' |_',sentence)))
+        difficultSyllableList = list(filter(lambda  x: x and not x in commonSyllableList,syllableList))
+        totalSyllable += len(syllableList)
+        totalDifficultSyllable += len(difficultSyllableList)
+    return totalDifficultSyllable/totalSyllable
+
+pdsIndex = calculatePDS(sentenceList,commonSyllableSet)
+print('pds',pdsIndex)
+
+# percertange of difficult word
+
+def calculatePDW(sentenceList,commonWordList):
+    global sentenceMarkList
+    totalWord = 0
+    totalDifficultWord = 0
+    for sentence in sentenceList:
+        wordList = list(filter(lambda x: not  x in sentenceMarkList ,sentence.split()))
+        difficultWordList = list(filter(lambda  x : not x.replace('_',' ') in commonWordList,wordList))
+        totalWord += len(wordList)
+        totalDifficultWord += len(difficultWordList)
+    return totalDifficultWord/totalWord
+
+pdwIndex = calculatePDW(sentenceList,commonWordSet)
+print('pdw',pdwIndex)
+
+#percertange of Sino-vietnamese word
+
+def calculatePSVW(sentenceList,sinoVietWordSet):
+    global sentenceMarkList
+    totalSinoVietWord = 0
+    totalWord = 0
+    for sentence in sentenceList:
+        wordList = list(filter(lambda  x: not x in sentenceMarkList, sentence.split()))
+        sinoVietWordList = list(filter(lambda x: x.replace('_',' ') in sinoVietWordSet,wordList))
+        totalSinoVietWord+=len(sinoVietWordList)
+        totalWord+=len(wordList)
+    return totalSinoVietWord/totalWord
+
+psvwIndex = calculatePSVW(sentenceList,sinoVietWordSet)
+print('psvw',psvwIndex)
+
+#percertange of dialect words (pdiaw)
+
+def calculatePDiaW(sentenceList,dialectWordSet):
+    global sentenceMarkList
+    totalDialectWord = 0
+    totalWord = 0
+    for sentence in sentenceList:
+        wordList = list(filter(lambda  x: not x in sentenceMarkList, sentence.split()))
+        dialectWordList = list(filter(lambda x: x.replace('_',' ') in dialectWordSet,wordList))
+        totalWord+=len(wordList)
+        totalDialectWord+=len(dialectWordList)
+    return totalDialectWord/totalWord
+
+pdiawIndex = calculatePDiaW(sentenceList,dialectWordSet)
+print('pdiaw',pdiawIndex)
+
 
 #calculate score
 
+def calculateLAVFomula(aslcIndex, awlcIndex, pdwIndex ):
+    return 0.004*aslsIndex + 0.1905*awlcIndex + 2.7147*pdwIndex - 0.7295
+ 
+def calculateNH1982(aslcIndex,awlcIndex):
+    return 2*awlcIndex + 0.2*aslcIndex - 6
+
+def calculateNH1985(aslcIndex, pswIndex):
+    #Vocabulary difficulty (WD)
+    wdIndex =  pswIndex*100
+    return 0.27*wdIndex + 0.13*aslcIndex +1.74
 
 #classification
+
+RL_LavFomula = calculateLAVFomula(aslcIndex,awlcIndex,pdwIndex)
+print("LAV",RL_LavFomula)
 
 
 commonWordFile.close()
 commonSyllableFile.close()
+sinoVietWordFile.close()
+dialectWordFile.close()
